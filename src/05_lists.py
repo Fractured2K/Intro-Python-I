77ba4a1106f40bf1612fc2366e5ff8e38add33f9
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-28 12:33:09 -0700

    Complete 05_lists exercises

--- src/05_lists.py
+++ src/05_lists.py
@@ -7,23 +7,26 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# YOUR CODE HERE 
-print(x)
+x.append(4)
+print("Add 4 to x list:", x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# YOUR CODE HERE 
-print(x)
+x.extend(y)
+print("Combine x and y lists:", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# YOUR CODE HERE 
-print(x)
+x.pop(4)
+print("Remove 8 from x list:", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# YOUR CODE HERE 
-print(x)
+x.insert(5, 99)
+print("Insert 99 into x list:", x)
 
 # Print the length of list x
-# YOUR CODE HERE 
+print("Length of x list", len(x))
 
 # Print all the values in x multiplied by 1000
-# YOUR CODE HERE
+print("Multiplied values", [i*1000 for i in x])  # Comprehension
+
+for i in x:
+    print(f"{i} multiplied by 1000", i * 1000)
